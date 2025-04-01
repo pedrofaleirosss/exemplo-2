fc1a58b9d2d2541fb501257404ec90fb0136be37
Pedro <pedro.alves.faleiros@gmail.com>
2025-04-01 09:10:04 -0300

    declaracao e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+// Declarações de Variáveis
+
+var nome = "Pedro";
+console.log(nome);
+
+let idade = 17;
+console.log(idade);
+
+const sobreNome = "Faleiros";
+console.log(sobreNome);
+
+let aula;
+console.log(aula);
+
+let valor = null;
+console.log(valor);
+
+let exemplo1 = {};
+
+let exemplo2 = [];
