313209da82d493ef25c025d02bd5dbe26022df90
Pedro <pedro.alves.faleiros@gmail.com>
2025-04-08 08:46:28 -0300

    adicionando operadores de comparacao

--- src/js/script.js
+++ src/js/script.js
@@ -1,91 +1,101 @@
 // Declarações de Variáveis
 
-var nome = "Pedro";
-console.log(nome);
+// var nome = "Pedro";
+// console.log(nome);
 
-let idade = 17;
-console.log(idade);
+// let idade = 17;
+// console.log(idade);
 
-const sobreNome = "Faleiros";
-console.log(sobreNome);
+// const sobreNome = "Faleiros";
+// console.log(sobreNome);
 
-let aula;
-console.log(aula);
+// let aula;
+// console.log(aula);
 
-let valor = null;
-console.log(valor);
+// let valor = null;
+// console.log(valor);
 
-let exemplo0 = false;
+// let exemplo0 = false;
 
-let exemplo1 = {};
+// let exemplo1 = {};
 
-let exemplo2 = [];
+// let exemplo2 = [];
 
 // Tipos de Variávies
 
-let exemplo3 = 10;
-console.log(typeof exemplo3);
+// let exemplo3 = 10;
+// console.log(typeof exemplo3);
 
-let exemplo4 = "Aula";
-console.log(typeof exemplo4);
+// let exemplo4 = "Aula";
+// console.log(typeof exemplo4);
 
-let exemplo5 = true;
-console.log(typeof exemplo5);
+// let exemplo5 = true;
+// console.log(typeof exemplo5);
 
-let exemplo6 = ["huguinho", "zezinho", "luizinho"];
-console.log(typeof exemplo6);
+// let exemplo6 = ["huguinho", "zezinho", "luizinho"];
+// console.log(typeof exemplo6);
 
-let exemplo7 = null;
-console.log(typeof exemplo7);
+// let exemplo7 = null;
+// console.log(typeof exemplo7);
 
 // Conversões
 
 // float => inteiro
-let numFloat = 123.456;
-console.log(parseInt(numFloat));
+// let numFloat = 123.456;
+// console.log(parseInt(numFloat));
 
 // string => float
-let numString = "547.281";
-console.log(parseFloat(numString));
+// let numString = "547.281";
+// console.log(parseFloat(numString));
 
 // float => string
-let numFloat2 = 314.424;
-console.log(numFloat2.toString());
-console.log(String(numFloat2));
+// let numFloat2 = 314.424;
+// console.log(numFloat2.toString());
+// console.log(String(numFloat2));
 
-let numInt = 100;
-console.log(numInt.toString());
+// let numInt = 100;
+// console.log(numInt.toString());
 
 // Métodos - Parte 1
 
 // length - Verifica o tamanho da String
-let frase = "O mundo da tecnologia";
-console.log(frase.length);
+// let frase = "O mundo da tecnologia";
+// console.log(frase.length);
 
 // indexOf - Retorna um trecho de um texto
-let texto = "Programação Sustentável";
-console.log(texto.indexOf("g"));
+// let texto = "Programação Sustentável";
+// console.log(texto.indexOf("g"));
 
 // slice - Retorna parte de um texto apontando o início e o final
-let info = "Processamento de ponta";
-console.log(info.slice(1, 5));
+// let info = "Processamento de ponta";
+// console.log(info.slice(1, 5));
 
 // Operadores Aritméticos
 
-const num1 = 10;
-const num2 = 20;
+// const num1 = 10;
+// const num2 = 20;
 
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
 
 // Operadores Lógicos
 
-const num3 = 30;
-const num4 = 40;
+// const num3 = 30;
+// const num4 = 40;
 
-console.log(num3 < num4);
-console.log(num3 < num4 && num4 > 50);
-console.log(num3 < num4 || num4 > 50);
-console.log(num3 == num4 || num4 <= num3);
+// console.log(num3 < num4);
+// console.log(num3 < num4 && num4 > 50);
+// console.log(num3 < num4 || num4 > 50);
+// console.log(num3 == num4 || num4 <= num3);
+
+// Operadores de Comparação
+
+const num5 = "35";
+const num6 = 35;
+
+console.log(num5 == num6); // Compara
+console.log(num5 === num6); // Compara e verifica o tipo da variável
+console.log(num5 != num6); // Diferente
+console.log(num5 !== num6); // Diferente e tipo
